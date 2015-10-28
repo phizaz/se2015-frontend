d7dce5bbbba6bee667f17c5aaad89b1ad33e8189
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-10-28 20:11:25 +0700

    development modue using dev-server and hot module replacement

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -5,13 +5,18 @@
 var path = require('path');
 
 module.exports = {
+    // this will be use as proxy for development server
+    // it will be called for backend
+    baseUrl: 'http://angular-seed.dev',
     entry: {
         app: ['./client/app/bootstrap.js'],
     },
     output: {
         path: path.resolve(__dirname, 'public'),
+
+        // The output.path from the view of the Javascript / HTML page.
         publicPath: '/public/',
-        filename: "[name].bundle.js",
+        filename: '[name].bundle.js',
     },
     module: {
         // this will be extended in gulp/webpack
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,5 +1,6 @@
 // webpack development config
 var _ = require('lodash');
+var webpack = require('webpack');
 
 var webpackBaseConfig = _.cloneDeep(require('./webpack.base.config'));
 
@@ -7,7 +8,39 @@ var webpackDevelopmentConfig = _.extend(webpackBaseConfig, {
   watch: true,
   // this is a faster but not as good sourcemap
   devtool: '#eval-cheap-module-source-map',
+
+  devServer: {
+    port: 3000,
+    // hot module replacement mode (default: true)
+    hot: true,
+
+    proxy: {
+      '/server/*': {
+        target: webpackBaseConfig.baseUrl + '/server',
+        secure: false,
+      },
+    },
+
+    // publicPath defines a path storing bundle.js
+    publicPath: webpackBaseConfig.output.publicPath,
+
+    quiet: false,
+    // noInfo: true,
+    stats: { colors: true },
+
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 });
+
+/**
+ * For Webpack's Hot Module Replacement
+ * add an entry point to the webpack configuration:
+ * 'webpack/hot/dev-server'
+ */
+webpackDevelopmentConfig.entry.app.unshift("webpack-dev-server/client?http://localhost:" + webpackDevelopmentConfig.devServer.port, "webpack/hot/dev-server");
 
 webpackDevelopmentConfig.module.loaders = [
   // babel (es6)
