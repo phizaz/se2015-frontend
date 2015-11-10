c5bd9f1ab166e20f22d8e37c8cf57630de694366
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-11-10 21:56:39 +0700

    better login box directive code style

--- client/app/components/login-box/login-box.directive.js
+++ client/app/components/login-box/login-box.directive.js
@@ -1,10 +1,39 @@
 import angular from 'angular';
+import _ from 'lodash';
 
 import loginBoxTemplate from './login-box.template.html';
 
-export class LoginBoxController {
+export class LoginBoxDirective {
   constructor() {
-    this.test = 'yo! this is the login-box';
+    _.extend(this, {
+      // this direcitve will apply to tag's name only,
+      // i.e. <login-box>, note that loginBox will be
+      // transformed to login-box
+      restrict: 'E',
+      // create its isolate scope that will not interfere with
+      // the outside world
+      // scope is equivalent to `this` in the class above
+      scope: {},
+      // always use bindToController
+      // so that the code will work as expected
+      bindToController: true,
+      // this is var's name to be used in template
+      // to use controller's `this`
+      controllerAs: 'loginBox',
+      template: loginBoxTemplate,
+    });
+  }
+
+  controller() {
+    console.log('controller executed!');
+  }
+
+  link(scope, element, attr) {
+    console.log('scope:', scope);
+    console.log('element:', element);
+    console.log('attr:', attr);
+
+    element.children('.modal').openModal();
   }
 }
 
@@ -13,22 +42,5 @@ export let loginBoxDirectiveModule =
 
 loginBoxDirectiveModule.directive('loginBox',
   () => {
-    return {
-      // this direcitve will apply to tag's name only,
-      // i.e. <login-box>, note that loginBox will be
-      // transformed to login-box
-      restrict: 'E',
-      // create its isolate scope that will not interfere with
-      // the outside world
-      scope: {},
-      // always use bindToController
-      // so that the code will work as expected
-      bindToController: true,
-      controller: LoginBoxController,
-      // this is var's name to be used in template
-      // to use controller's `this`
-      controllerAs: 'loginBox',
-      template: loginBoxTemplate,
-    };
-
+    return new LoginBoxDirective();
   });
