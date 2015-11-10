8586ceaa0ebbece09052681f4519c16327092834
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-11-10 21:42:23 +0700

    update navigator to support multiple pages

--- client/app/routes/navigator/navigator.route.js
+++ client/app/routes/navigator/navigator.route.js
@@ -1,10 +1,15 @@
 import angular from 'angular';
 import 'angular-ui-router';
 
+// routes
 import {homeRouteModule} from '../home/home.route.js';
+import {loginRouteModule} from '../login/login.route.js';
 
+// servicse
 import {navigatorServiceModule} from '../../services/navigator.service.js';
 import {messagerServiceModule} from '../../services/messager.service.js';
+
+// local use
 import {NavigatorController} from './navigator.controller.js';
 import navigatorTemplate from './navigator.template.html';
 import './navigator.sass';
@@ -12,7 +17,12 @@ import './navigator.sass';
 export let navigatorRouteModule =
   angular.module('navigatorRouteModule', [
     'ui.router',
+
+    // routes
     homeRouteModule.name,
+    loginRouteModule.name,
+
+    // services
     navigatorServiceModule.name,
     messagerServiceModule.name,
   ]);
