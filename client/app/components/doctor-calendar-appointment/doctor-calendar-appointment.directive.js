64b9f8e0e57dc265346a8ee6536e0fe6116d5d2e
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-11-17 18:21:54 +0700

    init doctor directives

---
+++ client/app/components/doctor-calendar-appointment/doctor-calendar-appointment.directive.js
@@ -0,0 +1,49 @@
+import angular from 'angular';
+import _ from 'lodash';
+import {DirectiveBlueprint} from '../directive';
+
+// locals
+import template from './doctor-calendar-appointment.template.html';
+import './doctor-calendar-appointment.sass';
+
+export let doctorCalendarAppointmentDirectiveModule =
+  angular
+    .module('doctorCalendarAppointmentDirectiveModule', [])
+    .directive('doctorCalendarAppointment', doctorCalendarAppointmentDirective);
+
+export function doctorCalendarAppointmentDirective() {
+
+  let shared = {};
+
+  function controller($scope) {
+    let my = DirectiveBlueprint.constructor($scope, this);
+
+    _.extend(my, {
+
+      // this is intentionally put here
+      public: my,
+    });
+  }
+
+  function link($scope, element, attrs) {
+    let my = DirectiveBlueprint.getPrivate($scope);
+
+    _.extend(my, {
+      element: element,
+      attrs: attrs,
+    });
+  }
+
+  return {
+    restrict: 'E',
+    scope: {
+
+    },
+    bindToController: true,
+    controller: controller,
+    controllerAs: 'my',
+    link: link,
+    template: template,
+  };
+
+}
