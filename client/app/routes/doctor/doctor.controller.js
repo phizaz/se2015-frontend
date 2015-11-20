64712f1b32d83a0f535efe6d320bdd576a1e585f
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2015-11-20 11:42:38 +0700

    improve doctor controller to support actions like nextWeek previousWeek

--- client/app/routes/doctor/doctor.controller.js
+++ client/app/routes/doctor/doctor.controller.js
@@ -1,28 +1,36 @@
 import _ from 'lodash';
+import moment from 'moment';
 
 export class DoctorController {
-  constructor(userInfo) {
+  constructor(userInfo, doctorTimeList) {
 
     console.log('userInfo:', userInfo);
 
-    let items = [
-      { sizeX: 2, sizeY: 1, row: 0, col: 0 },
-      { sizeX: 2, sizeY: 2, row: 0, col: 2 },
-      { sizeX: 1, sizeY: 1, row: 0, col: 4 },
-      { sizeX: 1, sizeY: 1, row: 0, col: 5 },
-      { sizeX: 2, sizeY: 1, row: 1, col: 0 },
-      { sizeX: 1, sizeY: 1, row: 1, col: 4 },
-      { sizeX: 1, sizeY: 2, row: 1, col: 5 },
-      { sizeX: 1, sizeY: 1, row: 2, col: 0 },
-      { sizeX: 2, sizeY: 1, row: 2, col: 1 },
-      { sizeX: 1, sizeY: 1, row: 2, col: 3 },
-      { sizeX: 1, sizeY: 1, row: 2, col: 4 }
-    ];
-
     _.extend(this, {
-      items: items,
+      doctorTimeList: doctorTimeList,
+      currentWeek: moment(),
     });
 
+  }
+
+  goNextWeek() {
+    this.currentWeek = this.currentWeek.add(1, 'w');
+  }
+
+  goPreviousWeek() {
+    this.currentWeek = this.currentWeek.substract(1, 'w');
+  }
+
+  goPresentWeek() {
+    this.currentWeek = moment();
+  }
+
+  goNextMonth() {
+    this.currentWeek = this.currentWeek.add(1, 'm');
+  }
+
+  goPreviousMonth() {
+    this.currentWeek = this.currentWeek.substract(1, 'm');
   }
 
 
